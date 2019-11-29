d2bcbcfce4337d905bc52bee36a1ebf57cc58968
Damian Obajtek <damian.obajtek@gmail.com>
2019-11-29 17:44:23 +0100

    better deleting

--- Projekt 10 - TO DO/main.js
+++ Projekt 10 - TO DO/main.js
@@ -1,5 +1,7 @@
 const removeTask = (e) => {
-    e.target.parentNode.style.textDecoration = "line-through";
+    const index = e.target.dataset.key;
+    document.querySelector(`li[data-key="${index}"]`).remove()
+
 }
 
 document.querySelectorAll(`li button`).forEach( item => {
