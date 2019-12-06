eca59e19b497da2ec330b08b9bad118af44dc40b
Damian Obajtek <damian.obajtek@gmail.com>
2019-12-07 00:06:36 +0100

    stats finished

--- Projekt 16 - sample  OOP/statistics.js
+++ Projekt 16 - sample  OOP/statistics.js
@@ -1,24 +1,22 @@
 class Statistics {
-    constructor() {
-        this.gameResults = [{win: true, bid: 2}];
-    }
+  constructor() {
+    this.gameResults = [{ win: true, bid: 2 }];
+  }
 
-    addGameToStats(win, bid) {
-        let gameResult = {
-            // win: win,
-            // bid: bid
-            win,
-            bid
-        } 
-        this.gameResults.push(gameResult)
-    }
+  addGameToStats(win, bid) {
+    let gameResult = {
+      // win: win,  // to samo
+      // bid: bid
+      win,
+      bid
+    };
+    this.gameResults.push(gameResult);
+  }
 
-    showGameStats() {
-        let games = this.gameResults.length;
-        let wins = this.gameResults.filter(result => result.win)
-        //return [games, liczbaWygranych, liczbaPorazek]
-    }
-
+  showGameStats() {
+    let games = this.gameResults.length;
+    let wins = this.gameResults.filter(result => result.win).length;
+    let losses = this.gameResults.filter(result => !result.win).length;
+    return [games, wins, losses];
+  }
 }
-
-const stats = new Statistics()
